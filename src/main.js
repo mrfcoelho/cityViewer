28a3b127c09a11835d4418b8197450ccf3419b25
mrfcoelho <mrfcoelho@gmail.com>
2025-01-02 15:17:59 +0000

    [FEAT] update

--- src/main.js
+++ src/main.js
@@ -16,16 +16,12 @@ const viewer = new Viewer("cesiumContainer");
 
 // add the button to change to AR
 var buttonEl = document.createElement("a");
-buttonEl.href = "www.google.com";
-buttonEl.className = "button-to-AR";
+buttonEl.href = "https://mrfcoelho.github.io/ifc2locar/";
+buttonEl.className = "cesium-button cesium-toolbar-button button-to-AR";
 buttonEl.innerText = "AR";
 document
   .getElementsByClassName("cesium-viewer-toolbar")[0]
   .appendChild(buttonEl);
-
-{
-  /* <button type="button" class="cesium-button cesium-toolbar-button cesium-home-button" data-bind="attr: { title: tooltip },click: command,cesiumSvgPath: { path: _svgPath, width: 28, height: 28 }" title="View Home"><svg:svg class="cesium-svgPath-svg" width="28" height="28" viewBox="0 0 28 28"><path d="M14,4l-10,8.75h20l-4.25-3.7188v-4.6562h-2.812v2.1875l-2.938-2.5625zm-7.0938,9.906v10.094h14.094v-10.094h-14.094zm2.1876,2.313h3.3122v4.25h-3.3122v-4.25zm5.8442,1.281h3.406v6.438h-3.406v-6.438z"></path></svg:svg></button> */
-}
 
 // Fly the camera to the given longitude, latitude, and height.
 viewer.camera.flyTo({
