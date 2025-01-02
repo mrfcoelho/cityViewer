40e48b9d4830fd213d1e4d781e3e6a6b783a5ae6
mrfcoelho <mrfcoelho@gmail.com>
2025-01-02 12:31:16 +0000

    [FEAT] update

--- vite.config.js
+++ vite.config.js
@@ -12,6 +12,7 @@ export default defineConfig({
     // Define relative base path in cesium for loading assets
     // https://vitejs.dev/config/shared-options.html#define
     CESIUM_BASE_URL: JSON.stringify(`/${cesiumBaseUrl}`),
+    base: "/cityViewer/",
   },
   plugins: [
     // Copy Cesium Assets, Widgets, and Workers to a static directory.
