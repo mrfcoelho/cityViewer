2691c9449816dee6f0bd9d20a98ab91fb7bb3bd9
mrfcoelho <mrfcoelho@gmail.com>
2025-01-02 14:00:41 +0000

    [FEAT] update

--- vite.config.js
+++ vite.config.js
@@ -4,14 +4,14 @@ import { viteStaticCopy } from "vite-plugin-static-copy";
 const cesiumSource = "node_modules/cesium/Build/Cesium";
 // This is the base url for static files that CesiumJS needs to load.
 // Set to an empty string to place the files at the site's root path
-const cesiumBaseUrl = "cityViewer/cesiumStatic";
+const cesiumBaseUrl = "cesiumStatic";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   define: {
     // Define relative base path in cesium for loading assets
     // https://vitejs.dev/config/shared-options.html#define
-    CESIUM_BASE_URL: JSON.stringify(`/${cesiumBaseUrl}`),
+    CESIUM_BASE_URL: JSON.stringify(`${cesiumBaseUrl}`),
   },
   plugins: [
     // Copy Cesium Assets, Widgets, and Workers to a static directory.
